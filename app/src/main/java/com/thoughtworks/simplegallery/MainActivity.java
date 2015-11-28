9b612509a503d2ccafa797c4b583ea882a44c554
Li Ang <liangsjob@gmail.com>
2015-11-28 14:00:43 +0800

    remove photo fragment

--- app/src/main/java/com/thoughtworks/simplegallery/MainActivity.java
+++ app/src/main/java/com/thoughtworks/simplegallery/MainActivity.java
@@ -1,26 +1,19 @@
 package com.thoughtworks.simplegallery;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.thoughtworks.simplegallery.fragment.GalleryFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG_FRAGMENT = "gallery_fragment";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        GalleryFragment galleryFragment = (GalleryFragment) fm.findFragmentByTag(TAG_FRAGMENT);
-
-        if (galleryFragment == null) {
-            galleryFragment = new GalleryFragment();
-            fm.beginTransaction().add(galleryFragment, TAG_FRAGMENT).commit();
-        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new GalleryFragment())
+                .commit();
     }
 }
